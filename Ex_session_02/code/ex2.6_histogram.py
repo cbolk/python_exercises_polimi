b80d5f2bba19584957cbe31418b093b66cd91450
cbolk <cristiana.bolchini@polimi.it>
2020-10-31 21:25:28 +0100

    Update ex2.6_histogram.py

--- Ex_session_02/code/ex2.6_histogram.py
+++ Ex_session_02/code/ex2.6_histogram.py
@@ -1,9 +1,9 @@
 # Your hospital uses a format to represent temperatures (expressed in Celsius integer degrees)
-# got during the 12 months of current year.
+# measured during the 12 months of the current year.
 # the format is fixed:
 # 5,6,7,12,16,25,26,22,18,10,5,5
-# Write a program that asks the user a string containing 12 temperatures, computing and outputting
-# an histogram plot of values using '*' for every 5 degrees, or False if string is not correct, or a value# is negative
+# Write a program that asks the user a string containing 12 temperatures, computes and outputs
+# a histogram plot of values using '*' for every 5 degrees, or False if string is not correct, or a value# is negative
 # the histogram spans from 0 to 30 degrees.
 # write the scale on left.
 # Don't use find
@@ -31,32 +31,32 @@ while i<len and not error:
     ch = temperatures[i]
     if ch == ",":
         s = temperatures[prev:i]
-        if not s.isdigit():
-            error = True
-        else:
+        if s.isdigit():
             t = int(s)
             prev=i+1
             month+=1
+        else:
+            error = True
     i += 1
 
-if prev<len:    # take last number:
+if not error:    # take last number:
     s = temperatures[prev:i]
-    if not s.isdigit():
-        error = True
-    else:
+    if s.isdigit():
         t = int(s)
         month += 1
+    else:
+        error = True
 
-if not error and month == 12:
+if not error and month == N_OF_MONTHS:
 # same loop but inside a loop for values between 0..30 in steps of 5:
 
-    for degrees in range(30, 0, -5):
+    for degrees in range(30, 0, -5):    
         line = '{:0>2}'.format(degrees)  + ": "
 
         # same loop but no more checks, string is valid:
         i=0
         prev=0
-        while i<len and not error:
+        while i<len and not error:  # e che errore potremmo avere?
             ch = temperatures[i]
             if ch == ",":
                 s = temperatures[prev:i]
@@ -68,13 +68,13 @@ if not error and month == 12:
                     line += " "
             i += 1
 
-        if prev<len:    # take last number:
-            s = temperatures[prev:i]
-            t = int(s)
-            if t >= degrees:
-                line += "*"
-            else:
-                line += " "
+        #perche' c'era un if che mi pareva essere sempre vero?
+        s = temperatures[prev:i]
+        t = int(s)
+        if t >= degrees:
+            line += "*"
+        else:
+            line += " "
 
         print(line)
 else:
